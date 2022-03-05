5bd7c70e4812449968c38f8c45e1f60b881f698e
Chris Chen <cchen@futuredial.com>
2022-03-05 10:17:27 -0800

    code update

--- pyideviceutil/pyideviceutil.py
+++ pyideviceutil/pyideviceutil.py
@@ -1,9 +1,23 @@
+import sys
 import click
-from test_cli import cli as test_cli1
+# from test_cli import cli as test_cli1
 from listdevice import cli as listdevice_cli
 from dotask import cli as dotask_cli
 
-cli = click.CommandCollection(sources=[listdevice_cli, dotask_cli, test_cli1])
+def run_pymobiledevice3():
+    import runpy
+    del sys.argv[1]
+    runpy.run_module("pymobiledevice3", run_name='__main__')
+
+def test():
+    pass
 
 if __name__ == '__main__':
-    cli()
+    cmd = sys.argv[1] if len(sys.argv)>1 else ''
+    if cmd == 'pymd':
+        run_pymobiledevice3()
+    elif cmd == 'test':
+        test()
+    else:
+        cli = click.CommandCollection(sources=[listdevice_cli, dotask_cli])
+        cli()
